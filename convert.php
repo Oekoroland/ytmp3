b5b5c0335fde2582e093c3857e5fc500cbcfe1c3
Henrik <henrik@hnrk.io>
2021-03-17 08:10:36 +0100

    Security fixes and english comments

--- convert.php
+++ convert.php
@@ -1,75 +1,76 @@
 <!DOCTYPE HTML>
 <?php
 	$request_method = $_SERVER['REQUEST_METHOD'];
-	$videoDownloadVerzeichnis = "/PATH/TO/DOWNLOADS/";
+	$videoDownloadVerzeichnis = "/path/to/ytmp3-downloads/";
+	$cookiesVerzeichnis = "/path/to/ytmp3-downloads-cookies.txt/";
 	if ($request_method == 'POST') {
-		//Maximal 10 Links sind erlaubt.
+		//Maximum of 10 links supported for now.
 		$linkvars = array("link1","link2","link3","link4","link5","link6","link7","link8","link9","link10");
-		//Prüfe, ob POST-Daten korrekt sind und mind. einen richtigen Wert haben.
+		//Check, if POST values are correct.
 		if (!count($_POST) > 10 || !empty($_POST) || count(array_intersect($linkvars, $_POST)) > 0){
-			//Counter zählt die Anzahl der validen Links.
+			//Counter counts the amount of valid links.
 			$counter = 0;
-			//Schneide jeden validen Link zurecht, um das Video herunterladen zu können.
-			//Erlaubt sind momentan: youtube.com & youtu.be Links.
-			//Unterstützte Links können beliebig innerhalb der for-Schleife erweitert werden.
+			//Cut links to their minimal required length.
+			//Allowed are currently: youtube.com & youtu.be.
+			//youtube-dl supported videolinks can be added here in the future.
 			for($i = 1; $i <= count($_POST); $i++) {
 				${"link" . $i} = $_POST['link'.$i];
-				if (substr(${"link" . $i}, 0, 32) === "https://www.youtube.com/watch?v=") {
+				if (substr(${"link" . $i}, 0, 32) === "https://www.youtube.com/watch?v=" && preg_match("/[0-9A-Za-z_-]{10}[048AEIMQUYcgkosw]/", substr(${"link" . $i}, 32, 11))) {
 					$counter++;
 					${"link" . $i} = substr(${"link" . $i},0,43);
 				}
-				elseif (substr(${"link" . $i}, 0, 17) === "https://youtu.be/") {
+				elseif (substr(${"link" . $i}, 0, 17) === "https://youtu.be/" && preg_match("/[0-9A-Za-z_-]{10}[048AEIMQUYcgkosw]/", substr(${"link" . $i}, 17, 11))) {
 					$counter++;
 					${"link" . $i} = substr(${"link" . $i},0,28);
 				}
 				else
 				{
-					die("Mindestens eine URL ist kein gültiges Youtube-Video: \"" . ${"link" . $i} . "\"</br>Die URL sollte folgendermaßen aussehen: https://www.youtube.com/watch?v=[VIDEO-ID] <b>ODER</b> https://youtu.be/[VIDEO-ID]</br><a href=\"index.html\">Back</a></br>");
+					die("At least one videolink is not correct: \"" . ${"link" . $i} . "\"</br>The link should look like this: https://www.youtube.com/watch?v=[VIDEO-ID] <b>OR</b> https://youtu.be/[VIDEO-ID]</br><a href=\"index.html\">Back</a></br>");
 				}
 			}
-			//Generiere eine eindeutige ID für die Sitzung. (Später für den Download wichtig!)
+			//Generate unique ID. 
 			$id = md5(uniqid().mt_rand());
-			//Erstelle, falls nicht vorhanden, das Video-Downloadverzeichnis mit der eindeutigen ID.
+			//Create download folder with unique ID.
 			mkdir($videoDownloadVerzeichnis.$id, 0755, true);
 			$linksForYTDL = "";
-			//Addiere alle validen YT-Links zusammen in eine Variable und trenne die Links mit einer Leertaste.
+			//Add links together and seperate by whitespaces.
 			for($j = 1; $j <= $counter; $j++) {
 				$linksForYTDL .= ${"link" . $j} . " ";
 			}
 			$downloadDirectory = $videoDownloadVerzeichnis.$id."/";
-			//Führe youtube-dl aus und konvertiere die Videos zu MP3s.
-			passthru("youtube-dl --cookies ".$videoDownloadVerzeichnis."cookies.txt --extract-audio --embed-thumbnail --restrict-filenames --audio-format mp3 --audio-quality 0 --output \"".$videoDownloadVerzeichnis.$id."/%(title)s.%(ext)s\" ".$linksForYTDL."> /dev/null");
-			//Prüfe die Anzahl der Dateien im Downloadverzeichnis.
-			$files = glob($downloadDirectory. '*.mp3');
+			//Execute youtube-dl on shell
+			passthru("python3 /usr/bin/youtube-dl --cookies ".$cookiesVerzeichnis."cookies.txt --extract-audio --restrict-filenames --audio-format mp3 --audio-quality 0 --output \"".$videoDownloadVerzeichnis.$id."/%(title)s.%(ext)s\" ".$linksForYTDL."> /dev/null");
+			//Check filecount in download directory.
+			$files = glob($downloadDirectory.'*.mp3');
 			if ($files !== false) {
 				$filecount = count($files);
-				//Falls die Anzahl der Dateien im Downloadverzeichnis größer als 1 ist, dann zippe die MP3s. Ansonsten lade die MP3 direkt herunter.
-				//Falls Videos denselben Titel haben, wird die Datei überschrieben. (TODO: Fix!)
+				//If more than one file, download zipped file.
+				//Videos with the same video title are overwritten by the last entry. (To avoid multiple downloads of the same video)
 				if ($filecount > 1) {
 					shell_exec("cd ".$videoDownloadVerzeichnis.$id." && zip -r ytmp3-download.zip *");
 					shell_exec("rm -rf -d ".$videoDownloadVerzeichnis.$id."/*.mp3");
-					echo "<body style=\"background-color:#212529\"><h1 style=\"text-align:center;color:white\">MP3s herunterladen</h1><div style=\"text-align:center\"><a href=\"download.php?type=multiple&id=".$id."\" target=\"_blank\"><button style=\"top:50%;width:270px;background-color:DodgerBlue;border:none;color:white;padding:12px 30px;cursor:pointer;font-size:20px\">Download</button></a></div></body>";
+					echo "<body style=\"background-color:#212529\"><h1 style=\"text-align:center;color:white\">Download MP3s</h1><div style=\"text-align:center\"><a href=\"download.php?type=multiple&id=".$id."\" target=\"_blank\"><button style=\"top:50%;width:270px;background-color:DodgerBlue;border:none;color:white;padding:12px 30px;cursor:pointer;font-size:20px\">Download</button></a></div></body>";
 				}
 				else
 				{
-					echo "<body style=\"background-color:#212529\"><h1 style=\"text-align:center;color:white\">MP3 herunterladen</h1><div style=\"text-align:center\"><a href=\"download.php?type=single&id=".$id."\" target=\"_blank\"><button style=\"top:50%;width:270px;background-color:DodgerBlue;border:none;color:white;padding:12px 30px;cursor:pointer;font-size:20px\">Download</button></a></div></body>";
+					echo "<body style=\"background-color:#212529\"><h1 style=\"text-align:center;color:white\">Download MP3</h1><div style=\"text-align:center\"><a href=\"download.php?type=single&id=".$id."\" target=\"_blank\"><button style=\"top:50%;width:270px;background-color:DodgerBlue;border:none;color:white;padding:12px 30px;cursor:pointer;font-size:20px\">Download</button></a></div></body>";
 				}
 			}
-			//Falls keine Dateien im Downloadverzeichnis sind, gib eine Fehlermeldung aus.
+			//No files in download directory
 			else
 			{
-				die("Keine Dateien gefunden!");
+				die("No files found in directory!");
 			}
 		}
-		//Gib eine Fehlermeldung aus, falls keine validen Links gesendet wurden.
+		//Throw error if no valid links found
 		else
 		{
-			die("Ein Fehler ist aufgetreten. Bitte erneut versuchen.</br><a href=\"index.html\">Back</a>");
+			die("An error occured. Please try again.</br><a href=\"index.html\">Back</a>");
 		}
 	}
 	else
 	{
-	// Nur POST erlaubt!
+	// Only allow POST
 		header('Location: index.html');
 	}
 ?>
